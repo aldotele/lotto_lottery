f5dafecbd455cbd2e0bb68609cd0f1fe1349d1ae
aldotele <aldo.telese@hotmail.it>
2020-11-24 16:57:42 +0100

    new lotto tables library

---
+++ lotto/lotto_tables.py
@@ -0,0 +1,56 @@
+def print_beam(width):
+    """
+    creates a beam of a given width
+    e.g. a beam of width 8 is: +------+
+    """
+    print('+', end='')
+    for i in range(width-2):
+        print('-', end='')
+    print('+')
+
+
+def print_row(width, string):
+    """
+    creates a row with centered text
+    @param width is the preferred width of the row
+    @param string is the string that has to appear inside the row
+    """
+    if width % 2 != 0:
+        width -= 1
+
+    margin = width - len(string) - 2
+    print('|' + ' '*(margin//2) + string + ' '*(margin//2) + '|')
+
+
+def print_ticket(ticket, ticket_n, width=50):
+    # making sure the width is even in order to improve layout
+    if width % 2 != 0:
+        width -= 1
+
+    print_beam(width)
+
+    # HEADER ROW
+    header_string = 'LOTTO Ticket {}'.format(ticket_n)
+    print_row(width, header_string)  
+
+    print_beam(width)
+
+    # BET-CITY ROW
+    bet = ticket.bet_type.bet_type
+    city = ticket.city.city
+    bet_city_string = '{} on {}'.format(bet, city)
+    if len(bet_city_string) % 2 != 0:
+        bet_city_string += ' '
+    print_row(width, bet_city_string)
+
+    print('|' + ' '*(width-2) + '|')
+
+    # NUMBERS ROW
+    numbers = ticket.numbers.numbers
+    str_sequence = [str(n) for n in numbers]
+    numbers_string = ' '.join(str_sequence)
+    if len(numbers_string) % 2 != 0:
+        numbers_string += ' '
+    print_row(width, numbers_string)
+
+    print_beam(width)
