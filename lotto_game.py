d378fa29bf9fe294c269c98d05aaaed6c0e92356
aldotele <aldo.telese@hotmail.it>
2020-12-09 09:47:10 +0100

    tickets amount as positional CLI argument

--- lotto_game.py
+++ lotto_game.py
@@ -15,7 +15,7 @@ def play_lotto(tickets_amount):
 
 def main():
     ap = argparse.ArgumentParser(description='Lotto bill with one or more tickets')
-    ap.add_argument("-n", type=int, help='amount of tickets', choices=[1, 2, 3, 4, 5])
+    ap.add_argument("n", type=int, help='amount of tickets', choices=[1, 2, 3, 4, 5])
     ap.add_argument('-v', '--verbose', help='introduction message', action='store_true')
     args = ap.parse_args()
     print()
@@ -29,30 +29,11 @@ def main():
         print('- For each ticket, you will be asked to enter a bet type,'
               ' which must be coherent with the amount of numbers.')
         print('- For each ticket, you will be asked to choose a city of extraction.')
-    else:
-        pass
 
-    tickets_amount = args.n
-    # making it possible to launch the program both by command line and input entering
-    if tickets_amount is None:
-        tickets_amount = input('how many tickets? 1  2  3  4  5\nType here: ')
-        while True:
-            try:
-                tickets_amount = int(tickets_amount)
-                if 1 <= tickets_amount <= 5:
-                    lotto_game = play_lotto(tickets_amount)
-                    break
-                else:
-                    print('NOT VALID: amount must be between 1 and 5.')
-                    tickets_amount = input('how many tickets? ')
-            except:
-                print('NOT VALID: amount must be an integer between 1 and 5.')
-                tickets_amount = input('how many tickets? ')
-    else:
-        print('\nYou have chosen to create {} Lotto tickets'.format(args.n))
-        print('Let\'s get started!')
-        # the lotto_game variable will store an instance of LottoManager
-        lotto_game = play_lotto(args.n)
+    print('\nYou have chosen to create {} Lotto tickets'.format(args.n))
+    print('Let\'s get started!')
+    # the lotto_game variable will store an instance of LottoManager
+    lotto_game = play_lotto(args.n)
 
     # printing an instance of LottoManager will invoke the instance __str__ method with the business logic of the game
     print(lotto_game)
